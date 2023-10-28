d7b1e63ef6587ebd1019de1dd0b749eb0074ed26
yaswanth14062003 <141607413+yaswanth14062003@users.noreply.github.com>
2023-10-28 16:01:08 +0530

    Add files via upload

---
+++ task-3/src/App.js
@@ -0,0 +1,11 @@
+import { Color } from './Components/Palette'; // Change 'palette' to 'Color'
+
+function App() {
+  return (
+    <div className="App">
+      <Color />
+    </div>
+  );
+}
+
+export default App;
